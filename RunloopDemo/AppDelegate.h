e0f76dc61df700427cfd62824e71498508fc354a
Founder <Founder@founderdemacbook-pro.local>
2016-01-18 16:18:28 +0800

    Initial Commit

---
+++ RunloopDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunloopDemo
+//
+//  Created by Founder on 16/1/18.
+//  Copyright © 2016年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunloopDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunloopDemo
+//
+//  Created by Founder on 16/1/18.
+//  Copyright © 2016年 Founder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
