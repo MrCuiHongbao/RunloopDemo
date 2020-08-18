d71661aa1a9af9bac8d246dfc6933b25389a61bd
cuihongbao <cuihongbao@jd.com>
2020-08-18 18:43:21 +0800

    LLLLL

--- RunloopDemo/main.m
+++ RunloopDemo/main.m
@@ -7,24 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+//#import "AppDelegate.h"
 
-//int main(int argc, char * argv[]) {
-//    @autoreleasepool {
-//        int a = 10;
-//        int b = 20;
-//        static int sum = 0;
-//        void (^block)() = ^ {
-//            sum = a+b;
-//        };
-//        block ();
-//        return 0;
-////        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-//    }
-//}
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        int a = 10;
+        int b = 20;
+        static int sum = 0;
+        void (^block)() = ^ {
+            sum = a+b;
+        };
+        block ();
+        return 0;
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
+//int main(int argc, char * argv[]) {
+//    @autoreleasepool {
+//
+//        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+//    }
+//}
